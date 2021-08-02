9e4bf719bf6ef305832ee0092fc9f1bfa4cf5784
bemakonate <bemakonate011@gmail.com>
2021-08-02 13:43:23 -0400

    Add project to git

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd4890c7feb3cff83e628e452bf8df17'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ccc6c39c-be79-440e-b42a-bbecc59674ad'
+};
